db7de2193d930d8afbe4195116f176bf11c50d09
Akashem06 <aryankashem@gmail.com>
2023-08-29 10:49:30 -0600

    Second Commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+
 const shortURL = require('./models/shortURL')
 const mongoose = require('mongoose');
 const cors = require('cors')
+dotenv.config()
     
 var path = require('path');
 
 const port = process.env.PORT || 5000
 
-mongoose.connect("mongodb+srv://aryankashem:204LXJsy49LJfTU1@urlshortner.uqklyio.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => app.listen(port, () => {console.log("Server is running on port " + port)}))
     .catch((error) => console.log(error));
 
